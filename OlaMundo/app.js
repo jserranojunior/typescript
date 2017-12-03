61e9df2e0f30e8bb5bb9fe5816eb641d17b7c282
Jorge <jorgeserranojunior@hotmail.com>
2017-12-03 15:21:25 -0200

    ola mundo e tipos

---
+++ OlaMundo/app.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var OlaMundo = /** @class */ (function () {
+    function OlaMundo() {
+    }
+    OlaMundo.prototype.digaOlarTodos = function () {
+        console.log("OLA PRIMEIRA CLASS EM TS 1");
+    };
+    return OlaMundo;
+}());
+exports.OlaMundo = OlaMundo;
+var olaMundo = new OlaMundo();
+olaMundo.digaOlarTodos();
+//# sourceMappingURL=app.js.map
---
+++ OlaMundo/app.ts
@@ -0,0 +1,8 @@
+export class OlaMundo{
+    digaOlarTodos() {
+        console.log("OLA PRIMEIRA CLASS EM TS 1")
+    }
+}
+
+let olaMundo = new OlaMundo();
+olaMundo.digaOlarTodos();
